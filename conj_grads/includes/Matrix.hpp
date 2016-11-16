0295850b7114c0958ce0fb6917735b8f95d4084b
Alexander <aleksandr_chori@mail.ru>
2016-11-17 01:19:27 +1000

    Update Matrix.hpp
    Overloaded operators for matrixes

--- conj_grads/includes/Matrix.hpp
+++ conj_grads/includes/Matrix.hpp
@@ -17,18 +17,26 @@ public:
 	Matrix() = default;
     ~Matrix();
 
-    vector<double>& operator[](int i);
-    friend istream& operator>>(istream& is, Matrix& x);
-    friend ostream& operator<<(ostream& os, Matrix& x);
+    vector<double>&  operator[](int i);
+	vector<double>   operator()(unsigned  i);
+    friend istream&  operator>>(istream& is, Matrix& x);
+    friend ostream&  operator<<(ostream& os, Matrix& x);
 	friend ifstream& operator>>(ifstream& is, Matrix& x);
 
-	vector<double> quadratize();
-	Matrix transpose();
-    unsigned int size();
+	Matrix			 operator*(Matrix& x);
+	vector<double>	 operator*(vector<double>& x);
+	Matrix			 operator*(double x);
+
+	vector<double>	 quadratize();
+	Matrix		     transpose();
+    unsigned int     size();
 protected:
     vector<vector<double>> tab;
 };
 
 vector<double> solve(Matrix a);
+
+vector<double> vsMul(vector<double>& lhs, double rhs);
+double scalarMul(vector<double>& lhs, vector<double>& rhs);
 
 #endif //NM_DIVSUB_MATRIX_HPP
